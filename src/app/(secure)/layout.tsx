97d3caadf162bf55498990f4dce089a71eecb5e7
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-12-19 00:40:26 +0100

    add navbar

--- src/app/(secure)/layout.tsx
+++ src/app/(secure)/layout.tsx
@@ -1,8 +1,15 @@
+import Navbar from '@/components/navbar';
+
 export default function SecureLayout({
 	children,
 }: Readonly<{
 	children: React.ReactNode;
 }>) {
 	// TODO: Check auth information here
-	return children;
+	return (
+		<>
+			<Navbar />
+			{children}
+		</>
+	);
 }
