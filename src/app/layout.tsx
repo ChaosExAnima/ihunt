29925916b542c2c49516492996b59393b5831832
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-12-18 23:12:05 +0100

    adds dark mode

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,20 +1,22 @@
-import type { Metadata } from 'next';
-import { Geist, Geist_Mono } from 'next/font/google';
-import './globals.css';
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/theme-provider";
 
 const geistSans = Geist({
-	variable: '--font-geist-sans',
-	subsets: ['latin'],
+	variable: "--font-geist-sans",
+	subsets: ["latin"],
 });
 
 const geistMono = Geist_Mono({
-	variable: '--font-geist-mono',
-	subsets: ['latin'],
+	variable: "--font-geist-mono",
+	subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
-	title: 'iHunt',
-	description: 'Start your hunt today',
+	title: "iHunt",
+	description: "Start your hunt today",
 };
 
 export default function RootLayout({
@@ -23,11 +25,25 @@ export default function RootLayout({
 	children: React.ReactNode;
 }>) {
 	return (
-		<html lang="en">
+		<html lang="en" suppressHydrationWarning>
+			<head />
 			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+				className={cn(
+					geistSans.variable,
+					geistMono.variable,
+					"antialiased bg-stone-200 dark:bg-stone-900",
+				)}
 			>
-				{children}
+				<ThemeProvider
+					attribute="class"
+					defaultTheme="system"
+					enableSystem
+					disableTransitionOnChange
+				>
+					<main className="mx-auto mt-4 max-w-[360px] min-h-[687px] bg-background p-4">
+						{children}
+					</main>
+				</ThemeProvider>
 			</body>
 		</html>
 	);
