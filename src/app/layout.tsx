a80b9a73ee01deca2b00fa4987b90e9d2bf0cd8f
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-12-18 23:31:08 +0100

    fix up styles to use CSS variables

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,22 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { cn } from "@/lib/utils";
-import { ThemeProvider } from "@/components/theme-provider";
+import type { Metadata } from 'next';
+import { Geist, Geist_Mono } from 'next/font/google';
+import './globals.css';
+import { cn } from '@/lib/utils';
+import { ThemeProvider } from '@/components/theme-provider';
 
 const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
+	variable: '--font-geist-sans',
+	subsets: ['latin'],
 });
 
 const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
+	variable: '--font-geist-mono',
+	subsets: ['latin'],
 });
 
 export const metadata: Metadata = {
-	title: "iHunt",
-	description: "Start your hunt today",
+	title: 'iHunt',
+	description: 'Start your hunt today',
 };
 
 export default function RootLayout({
@@ -31,7 +31,7 @@ export default function RootLayout({
 				className={cn(
 					geistSans.variable,
 					geistMono.variable,
-					"antialiased bg-white dark:bg-black",
+					'antialiased bg-white dark:bg-black',
 				)}
 			>
 				<ThemeProvider
@@ -42,10 +42,10 @@ export default function RootLayout({
 				>
 					<main
 						className={cn(
-							process.env.NODE_ENV === "development" &&
-								"max-w-[360px] min-h-[687px]",
-							"mx-auto mt-4 p-4",
-							"bg-stone-200 dark:bg-stone-900",
+							process.env.NODE_ENV === 'development' &&
+								'max-w-[360px] min-h-[687px]',
+							'mx-auto mt-4 p-4',
+							'bg-background',
 						)}
 					>
 						{children}
