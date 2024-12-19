9e919fe5e2691b0a9775a622f47eb4d4c6ea68d5
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2024-12-19 18:26:15 +0100

    loads old hunts

--- src/app/(secure)/hunts/page.tsx
+++ src/app/(secure)/hunts/page.tsx
@@ -1,25 +1,39 @@
 import { HuntStatus } from '@/lib/constants';
 import { db } from '@/lib/db';
+import { fetchCurrentUser } from '@/lib/user';
 import Link from 'next/link';
 
 export default async function HuntsPage() {
-	const hunts = await db.hunt.findMany({
-		where: {
-			status: HuntStatus.Available,
-		},
-		include: {
-			_count: {
-				select: {
-					hunters: true,
+	const hunter = await fetchCurrentUser();
+	const [availableHunts, oldHunts] = await Promise.all([
+		db.hunt.findMany({
+			where: {
+				status: HuntStatus.Available,
+			},
+			include: {
+				_count: {
+					select: {
+						hunters: true,
+					},
 				},
 			},
-		},
-	});
+		}),
+		db.hunt.findMany({
+			where: {
+				status: HuntStatus.Complete,
+				hunters: {
+					some: {
+						hunter,
+					},
+				},
+			},
+		}),
+	]);
 	return (
 		<>
-			<h1 className="text-4xl font-bold mb-4">Available Hunts</h1>
+			<h2 className="text-4xl font-bold mb-4">Available Hunts</h2>
 			<ul className="flex flex-col gap-4">
-				{hunts.map((hunt) => (
+				{availableHunts.map((hunt) => (
 					<li key={hunt.id}>
 						<Link href={`/hunts/${hunt.id}`}>
 							<p className="font-bold">{hunt.description}</p>
@@ -27,6 +41,16 @@ export default async function HuntsPage() {
 								Spots available:&nbsp;
 								{hunt.maxHunters - hunt._count.hunters}
 							</p>
+						</Link>
+					</li>
+				))}
+			</ul>
+			<h2 className="text-4xl font-bold my-4">Previous Hunts</h2>
+			<ul className="flex flex-col gap-4">
+				{oldHunts.map((hunt) => (
+					<li key={hunt.id}>
+						<Link href={`/hunts/${hunt.id}`}>
+							<p className="font-bold">{hunt.description}</p>
 						</Link>
 					</li>
 				))}
