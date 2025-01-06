d54d217245e8c983a0d3c5f04bf099555a7a5ac4
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-01-06 17:23:42 +0100

    custom B2 upload

--- src/lib/photo.ts
+++ src/lib/photo.ts
@@ -1,23 +1,12 @@
 'use server';
 
-import B2 from 'backblaze-b2';
 import { fileTypeFromBuffer } from 'file-type';
 import { imageDimensionsFromData } from 'image-dimensions';
 import { createHash } from 'node:crypto';
-import z from 'zod';
 
+import { b2 } from './b2';
 import { db } from './db';
 import { fetchBlurry } from './image-loader';
-
-const b2UploadUrlResponse = z.object({
-	authorizationToken: z.string(),
-	bucketId: z.string(),
-	uploadUrl: z.string(),
-});
-
-const b2UploadResponse = z.object({
-	fileName: z.string(),
-});
 
 interface UploadPhotoArgs {
 	buffer: Uint8Array;
@@ -45,32 +34,14 @@ export async function uploadPhoto({
 		throw new Error('Could not extract dimensions');
 	}
 
-	// Hash buffer
+	// Hash buffer for the filename
 	const hash = createHash('sha256');
 	hash.update(arrayBuffer);
 	const hex = hash.digest('hex');
+	const fileName = `${hex}.${fileType.ext}`;
 
 	// Send it to B2
-	const b2 = new B2({
-		applicationKey: process.env.BACKBLAZE_BUCKET ?? '',
-		applicationKeyId: process.env.BACKBLAZE_KEY ?? '',
-	});
-	await b2.authorize();
-
-	const { data: urlData } = await b2.getUploadUrl({
-		bucketId: process.env.BACKBLAZE_BUCKET ?? '',
-	});
-
-	const { authorizationToken, uploadUrl } =
-		b2UploadUrlResponse.parse(urlData);
-
-	const { data } = await b2.uploadFile({
-		data: arrayBuffer,
-		fileName: `${hex}.${fileType.ext}`,
-		uploadAuthToken: authorizationToken,
-		uploadUrl: uploadUrl,
-	});
-	const { fileName } = b2UploadResponse.parse(data); // Use B2 file name
+	await b2.upload(buffer, fileName, fileType.mime);
 
 	// Fetch blurry version using Cloudflare image transforms
 	const blurryData = await fetchBlurry(fileName);
