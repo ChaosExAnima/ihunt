f1bf2d3cbb10c0bc0fb0e8c6c3a6ce45b89a840a
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-02-03 13:11:51 +0100

    feat: add nodemailer provider for email authentication in NextAuth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,65 +1,20 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
-import { Provider } from 'next-auth/providers';
-import Credentials from 'next-auth/providers/credentials';
 import Discord from 'next-auth/providers/discord';
+import NodeEmailer from 'next-auth/providers/nodemailer';
 
 import { db } from './db';
 import { isDev } from './utils';
 
-const providers: Provider[] = [Discord];
-const devPassword = process.env.ADMIN_PASSWORD;
-if (isDev() && devPassword) {
-	providers.push(
-		Credentials({
-			async authorize({ password }) {
-				try {
-					console.log(
-						'Authorizing with dev password:',
-						password !== devPassword ? password : '***',
-					);
-
-					if (password !== devPassword) {
-						throw new Error('Wrong password');
-					}
-					let user = await db.user.findFirst({
-						where: { id: 'admin' },
-					});
-					if (!user) {
-						user = await db.user.create({
-							data: { id: 'admin', name: 'Admin' },
-						});
-						await db.account.create({
-							data: {
-								provider: 'credentials',
-								providerAccountId: user.id,
-								type: 'credentials',
-								userId: user.id,
-							},
-						});
-					}
-					return user;
-				} catch (err) {
-					console.error(err);
-					return null;
-				}
-			},
-			credentials: {
-				password: {
-					label: 'Dev Password',
-					type: 'password',
-				},
-			},
-			name: 'dev password',
-		}),
-	);
-}
-
 export const { auth, handlers, signIn, signOut } = NextAuth({
 	adapter: PrismaAdapter(db),
 	debug: isDev(),
-	providers,
-	session: {
-		strategy: 'jwt',
-	},
+	providers: [
+		Discord,
+		NodeEmailer({
+			from: process.env.EMAIL_FROM,
+			name: 'email',
+			server: process.env.EMAIL_SERVER,
+		}),
+	],
 });
