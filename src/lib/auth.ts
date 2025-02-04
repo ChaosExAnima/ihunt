38248eabf21d988f158d09b8c21725b28dbc8b62
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-02-04 13:15:51 +0100

    feat: add conditional nodemailer provider to NextAuth configuration

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,20 +1,25 @@
 import { PrismaAdapter } from '@auth/prisma-adapter';
 import NextAuth from 'next-auth';
+import { Provider } from 'next-auth/providers';
 import Discord from 'next-auth/providers/discord';
 import NodeEmailer from 'next-auth/providers/nodemailer';
 
 import { db } from './db';
 import { isDev } from './utils';
 
-export const { auth, handlers, signIn, signOut } = NextAuth({
-	adapter: PrismaAdapter(db),
-	debug: isDev(),
-	providers: [
-		Discord,
+const providers: Provider[] = [Discord];
+if (process.env.EMAIL_SERVER && process.env.EMAIL_FROM) {
+	providers.push(
 		NodeEmailer({
 			from: process.env.EMAIL_FROM,
 			name: 'email',
 			server: process.env.EMAIL_SERVER,
 		}),
-	],
+	);
+}
+
+export const { auth, handlers, signIn, signOut } = NextAuth({
+	adapter: PrismaAdapter(db),
+	debug: isDev(),
+	providers,
 });
