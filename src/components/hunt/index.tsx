742345f571806a6ff3225b51db3a2d96fa38dbdc
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-02-04 23:54:05 +0100

    simplifies Hunt card

--- src/components/hunt/index.tsx
+++ src/components/hunt/index.tsx
@@ -1,16 +1,14 @@
 'use client';
 
-import { CircleCheckBig, Crosshair } from 'lucide-react';
+import { CircleCheckBig, Crosshair, X } from 'lucide-react';
+import { useMemo } from 'react';
 
 import { HuntModel, HuntStatus } from '@/lib/constants';
+import { acceptHunt } from '@/lib/hunt';
 import { cn } from '@/lib/utils';
 
-import DateDisplay from '../date';
-import Header from '../header';
-import { Card } from '../ui/card';
-import AvailableHunt from './available';
-import HuntHuntersDisplay from './hunters';
-import HuntPhotoDisplay from './photos';
+import { Button } from '../ui/button';
+import HuntBase from './base';
 
 export interface HuntProps {
 	className?: string;
@@ -19,48 +17,70 @@ export interface HuntProps {
 }
 
 export default function HuntDisplay(props: HuntProps) {
-	const { className, hunt, hunterId } = props;
-	if (hunt.status === HuntStatus.Available) {
-		return <AvailableHunt {...props} />;
-	} else if (hunt.status === HuntStatus.Cancelled) {
-		return null;
-	}
-	return (
-		<Card className={cn(className, 'flex flex-col')} slot="article">
-			<HuntPhotoDisplay photos={hunt.photos} />
-			<p className="text-stone-600 text-xs mb-2">
-				{hunt.status === HuntStatus.Complete ? 'finished' : 'added'}
-				&nbsp;
-				<DateDisplay date={hunt.completedAt || hunt.createdAt} />
-			</p>
-			<Header level={3}>{hunt.name}</Header>
-			<p className="my-4 grow">{hunt.description}</p>
-			<HuntHuntersDisplay
-				hunterId={hunterId}
-				hunters={hunt.hunters}
-				maxHunters={hunt.maxHunters}
-			/>
-			{hunt.status === HuntStatus.Active && (
-				<div className="flex mt-4 gap-2 items-center justify-center text-blue-500 text-center font-semibold">
-					<Crosshair className="size-4 shrink-0" />
-					Ongoing
-				</div>
-			)}
-			{hunt.status === HuntStatus.Complete && (
-				<div
-					className={cn(
-						'flex mt-4 gap-2 items-center justify-center font-semibold self-center',
-						'text-green-500',
-					)}
-				>
-					<CircleCheckBig
-						aria-label="Completed Hunt"
-						className="size-4 shrink-0"
-						strokeWidth="3"
-					/>
-					Complete!
-				</div>
-			)}
-		</Card>
+	const { hunt, hunterId } = props;
+	const isAccepted = useMemo(
+		() => hunt.hunters.some((hunter) => hunter.id === hunterId),
+		[hunt.hunters, hunterId],
 	);
+	switch (hunt.status) {
+		case HuntStatus.Active:
+			return (
+				<HuntBase {...props} isAccepted={isAccepted}>
+					<div className="flex mt-4 gap-2 items-center justify-center text-blue-500 text-center font-semibold">
+						<Crosshair className="size-4 shrink-0" />
+						Ongoing
+					</div>
+				</HuntBase>
+			);
+		case HuntStatus.Available:
+			return (
+				<HuntBase {...props} isAccepted={isAccepted}>
+					{hunt.maxHunters - hunt.hunters.length > 0 &&
+						!isAccepted && (
+							<p className="text-center text-sm">
+								You have 2 hunts left today.
+								<br />
+								<strong className="text-green-500">
+									Buy iHunt Premium to unlock more!
+								</strong>
+							</p>
+						)}
+					<Button
+						className="flex mx-auto rounded-full font-bold self-center"
+						onClick={() => acceptHunt(hunt.id)}
+						variant={isAccepted ? 'destructive' : 'success'}
+					>
+						{isAccepted ? (
+							<X />
+						) : (
+							<CircleCheckBig
+								aria-label="Accept hunt"
+								strokeWidth="3"
+							/>
+						)}
+						{isAccepted ? 'Cancel' : 'Accept'}
+					</Button>
+				</HuntBase>
+			);
+		case HuntStatus.Complete:
+			return (
+				<HuntBase {...props} isAccepted={isAccepted}>
+					<div
+						className={cn(
+							'flex mt-4 gap-2 items-center justify-center font-semibold self-center',
+							'text-green-500',
+						)}
+					>
+						<CircleCheckBig
+							aria-label="Completed Hunt"
+							className="size-4 shrink-0"
+							strokeWidth="3"
+						/>
+						Complete!
+					</div>
+				</HuntBase>
+			);
+		default:
+			return null;
+	}
 }
