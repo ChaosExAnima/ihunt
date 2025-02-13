0ecf5b96590693e1ea49774f3054cb7260248c9c
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-02-13 20:06:59 +0100

    Refactor SecureLayout to streamline session handling and improve error logging

--- src/app/(secure)/layout.tsx
+++ src/app/(secure)/layout.tsx
@@ -11,40 +11,39 @@ export default async function SecureLayout({
 	children: React.ReactNode;
 }>) {
 	await ensureLoggedIn();
-	let hunter;
 	try {
-		hunter = await sessionToHunter();
+		const hunter = await sessionToHunter();
+		const devMode = isDev();
+		return (
+			<div
+				className={cn(
+					'grow flex flex-col w-full justify-stretch',
+					devMode && 'border border-stone-400 dark:border-stone-800',
+					devMode && 'w-[360px] h-[687px] mx-auto mt-4',
+				)}
+			>
+				<Navbar hunter={hunter} />
+				<main className="grow px-4 flex flex-col gap-2 pb-4">
+					{children}
+				</main>
+			</div>
+		);
 	} catch (err) {
 		console.error('Error with session:', err);
-		return (
-			<main className="p-4 flex flex-col gap-4 text-center max-w-(--breakpoint-sm) mx-auto">
-				<Header level={1}>No hunter</Header>
-				<p>You do not have a hunter assigned yet.</p>
-				<Button
-					onClick={async () => {
-						'use server';
-						await signOut({ redirectTo: '/' });
-					}}
-					variant="secondary"
-				>
-					Log out
-				</Button>
-			</main>
-		);
 	}
-	const devMode = isDev();
 	return (
-		<div
-			className={cn(
-				'grow flex flex-col w-full justify-stretch',
-				devMode && 'border border-stone-400 dark:border-stone-800',
-				devMode && 'w-[360px] h-[687px] mx-auto mt-4',
-			)}
-		>
-			<Navbar hunter={hunter} />
-			<main className="grow px-4 flex flex-col gap-2 pb-4">
-				{children}
-			</main>
-		</div>
+		<main className="p-4 flex flex-col gap-4 text-center max-w-(--breakpoint-sm) mx-auto">
+			<Header level={1}>No hunter</Header>
+			<p>You do not have a hunter assigned yet.</p>
+			<Button
+				onClick={async () => {
+					'use server';
+					await signOut({ redirectTo: '/' });
+				}}
+				variant="secondary"
+			>
+				Log out
+			</Button>
+		</main>
 	);
 }
