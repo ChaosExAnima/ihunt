c5ad116b15100dc9bd3d6c6b82dd73d09978f5ca
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-02-13 23:29:51 +0100

    Enhance Hunter and Hunt APIs with improved data handling and error management

--- src/app/admin/api/hunter/route.ts
+++ src/app/admin/api/hunter/route.ts
@@ -5,45 +5,63 @@ import {
 	getListHandler,
 	getOneHandler,
 	RaPayload,
+	updateHandler,
 } from 'ra-data-simple-prisma';
 
 import { db } from '@/lib/db';
 
 const route = async (req: Request) => {
-	const body: RaPayload<Prisma.ModelName> = await req.json();
+	try {
+		const body: RaPayload<'Hunter'> = await req.json();
 
-	switch (body.method) {
-		case 'getList': {
-			const result = await getListHandler<Prisma.HunterFindManyArgs>(
-				body,
-				db.hunter,
-				{
-					include: {
-						avatar: true,
-						hunts: true,
-						user: true,
+		switch (body.method) {
+			case 'getList': {
+				const result = await getListHandler<Prisma.HunterFindManyArgs>(
+					body,
+					db.hunter,
+					{
+						include: {
+							avatar: true,
+							hunts: true,
+							user: true,
+						},
 					},
-				},
-			);
-			return NextResponse.json(result);
-		}
-		case 'getOne': {
-			const result = await getOneHandler<Prisma.HunterFindUniqueArgs>(
-				body,
-				db.hunter,
-				{
-					include: {
-						avatar: true,
-						photos: true,
-						user: true,
+				);
+				return NextResponse.json(result);
+			}
+			case 'getOne': {
+				const result = await getOneHandler<Prisma.HunterFindUniqueArgs>(
+					body,
+					db.hunter,
+					{
+						include: {
+							avatar: true,
+							photos: true,
+							user: true,
+						},
 					},
-				},
-			);
-			return NextResponse.json(result);
+				);
+				return NextResponse.json(result);
+			}
+			case 'update': {
+				const result = await updateHandler<Prisma.HunterUpdateArgs>(
+					body,
+					db.hunter,
+				);
+				return NextResponse.json(result);
+			}
+			default: {
+				const result = await defaultHandler(body, db);
+				return NextResponse.json(result);
+			}
 		}
-		default: {
-			const result = await defaultHandler(body, db);
-			return NextResponse.json(result);
+	} catch (err) {
+		if (err instanceof Error) {
+			console.error(err.stack);
+			return NextResponse.json({
+				error: true,
+				message: err.message,
+			});
 		}
 	}
 };
