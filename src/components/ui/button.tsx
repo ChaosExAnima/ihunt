960df3206fab2e601881f629126e72ab7f90e10b
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-03-05 17:08:42 +0100

    add cursor and clean up button classes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,7 +5,16 @@ import * as React from 'react';
 import { cn } from '@/lib/utils';
 
 const buttonVariants = cva(
-	'inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+	[
+		// Layout
+		'inline-flex items-center justify-center gap-2 rounded-md',
+		// Typography
+		'text-sm font-medium whitespace-nowrap',
+		// States
+		'transition-colors focus-visible:outline-hidden focus-visible:ring-1 focus-visible:ring-ring cursor-pointer',
+		// Disabled
+		'disabled:pointer-events-none disabled:opacity-50 disabled:cursor-auto [&_svg]:pointer-events-none [&_svg]:size-4 [&_svg]:shrink-0',
+	],
 	{
 		defaultVariants: {
 			size: 'default',
