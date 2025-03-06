1749e63244e07063d7395a3e6267a13ab419ef95
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-03-06 13:13:31 +0100

    Add pic management for active hunts

--- src/components/hunt/active.tsx
+++ src/components/hunt/active.tsx
@@ -1,15 +1,10 @@
-import { useMutation, useQueryClient } from '@tanstack/react-query';
-import { Crosshair, Trash } from 'lucide-react';
-import { useEffect, useMemo, useState } from 'react';
+import { Crosshair } from 'lucide-react';
+import { useState } from 'react';
 
-import { HunterSchema, HuntSchema, PhotoSchema } from '@/lib/schemas';
-import { cn } from '@/lib/utils';
+import { HuntSchema } from '@/lib/schemas';
 
-import Avatar from '../avatar';
-import PhotoDisplay from '../photo';
-import { Button } from '../ui/button';
 import HuntBase from './base';
-import { HuntPics as HuntPicsPicker } from './pics';
+import { HuntPics } from './pics';
 
 interface HuntDisplayActiveProps {
 	hunt: HuntSchema;
@@ -28,12 +23,14 @@ export function HuntDisplayActive({
 	return (
 		<HuntBase
 			afterHeader={
-				<HuntPicPicker
-					activeIndex={index}
-					hunt={hunt}
-					hunterId={hunterId}
-					onPick={handlePicPick}
-				/>
+				isAccepted && (
+					<HuntPics
+						activeIndex={index}
+						hunt={hunt}
+						hunterId={hunterId}
+						onPick={handlePicPick}
+					/>
+				)
 			}
 			className="mx-4"
 			hideHeader={index > 0}
@@ -41,116 +38,10 @@ export function HuntDisplayActive({
 			hunterId={hunterId}
 			isAccepted={isAccepted}
 		>
-			{isAccepted && <HuntPicsPicker huntId={hunt.id} />}
 			<div className="flex mt-4 gap-2 items-center justify-center text-rose-700 text-center font-semibold">
 				<Crosshair className="size-4 shrink-0" />
 				{isAccepted ? 'Good hunting!' : 'Ongoing'}
 			</div>
 		</HuntBase>
-	);
-}
-
-type HuntPicsPicker = {
-	activeIndex: number;
-	onPick: (index: number) => void;
-};
-
-function HuntPicPicker({
-	activeIndex,
-	hunt,
-	hunterId,
-	onPick,
-}: HuntPicsPicker & Pick<HuntDisplayActiveProps, 'hunt' | 'hunterId'>) {
-	const photos = hunt.photos;
-	const currentPhoto = photos[activeIndex];
-
-	const showPhoto = activeIndex >= 1;
-
-	useEffect(() => {
-		if (!currentPhoto && activeIndex >= 1) {
-			onPick(0);
-		}
-	}, [activeIndex, currentPhoto, onPick]);
-
-	if (photos.length <= 1) {
-		return null;
-	}
-	return (
-		<div className="">
-			{!!currentPhoto && showPhoto && (
-				<ActivePhoto
-					hunterId={hunterId}
-					hunters={hunt.hunters}
-					huntId={hunt.id}
-					photo={currentPhoto}
-				/>
-			)}
-			<div className="flex gap-2">
-				{photos.map((photo, index) => (
-					<PhotoDisplay
-						className={cn(
-							'size-10 rounded-md cursor-pointer border border-transparent',
-							index === activeIndex && 'border-rose-700',
-						)}
-						key={photo.id}
-						onClick={() => onPick(index)}
-						photo={photo}
-					/>
-				))}
-			</div>
-		</div>
-	);
-}
-
-interface ActivePhotoProps {
-	hunterId: number;
-	hunters: HunterSchema[];
-	huntId: number;
-	photo: PhotoSchema;
-}
-function ActivePhoto({ hunterId, hunters, huntId, photo }: ActivePhotoProps) {
-	const currentHunter = useMemo(
-		() => hunters.find((hunter) => hunter.id === photo.hunterId),
-		[photo.hunterId, hunters],
-	);
-	const isCurrentHunter = currentHunter?.id === hunterId;
-	return (
-		<div className="mb-2 rounded-md overflow-hidden relative">
-			<PhotoDisplay photo={photo} />
-			{!!currentHunter && !isCurrentHunter && (
-				<span className="absolute right-0 bottom-0 p-2 text-white text-sm flex gap-2 items-center bg-black/40 rounded-tl-md">
-					Uploaded by:
-					<Avatar hunter={currentHunter} link />
-				</span>
-			)}
-			{isCurrentHunter && (
-				<DeletePhotoButton huntId={huntId} photoId={photo.id} />
-			)}
-		</div>
-	);
-}
-
-interface DeletePhotoButtonProps {
-	huntId: number;
-	photoId: number;
-}
-function DeletePhotoButton({ huntId, photoId }: DeletePhotoButtonProps) {
-	const queryClient = useQueryClient();
-	const { mutate } = useMutation({
-		mutationFn: () =>
-			fetch(`/api/hunts/${huntId}/photos?photoId=${photoId}`, {
-				method: 'DELETE',
-			}),
-		onSuccess: () => queryClient.invalidateQueries({ queryKey: ['hunts'] }),
-	});
-	return (
-		<Button
-			className="absolute right-2 bottom-2"
-			onClick={() => mutate()}
-			size="icon"
-			variant="destructive"
-		>
-			<Trash />
-		</Button>
 	);
 }
--- src/components/hunt/pics.tsx
+++ src/components/hunt/pics.tsx
@@ -1,14 +1,128 @@
-import { useQueryClient } from '@tanstack/react-query';
-import { Camera } from 'lucide-react';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+import { Camera, Trash } from 'lucide-react';
 import { useCallback } from 'react';
+import { useEffect, useMemo } from 'react';
 import { z } from 'zod';
 
 import { fetchFromApi } from '@/lib/api';
+import { HunterSchema, PhotoSchema } from '@/lib/schemas';
+import { cn } from '@/lib/utils';
 
+import Avatar from '../avatar';
+import PhotoDisplay from '../photo';
 import { Button } from '../ui/button';
 import UploadPhoto from '../upload-photo';
+import { HuntDisplayProps } from './index';
 
-export function HuntPics({ huntId }: { huntId: number }) {
+interface HuntPics {
+	activeIndex: number;
+	onPick: (index: number) => void;
+}
+
+export function HuntPics({
+	activeIndex,
+	hunt,
+	hunterId,
+	onPick,
+}: HuntPics & Pick<HuntDisplayProps, 'hunt' | 'hunterId'>) {
+	const photos = hunt.photos;
+	const currentPhoto = photos[activeIndex];
+
+	const showPhoto = activeIndex >= 1;
+
+	useEffect(() => {
+		if (!currentPhoto && activeIndex >= 1) {
+			onPick(0);
+		}
+	}, [activeIndex, currentPhoto, onPick]);
+
+	if (photos.length <= 1) {
+		return null;
+	}
+	return (
+		<div className="">
+			{!!currentPhoto && showPhoto && (
+				<ActivePhoto
+					hunterId={hunterId}
+					hunters={hunt.hunters}
+					huntId={hunt.id}
+					photo={currentPhoto}
+				/>
+			)}
+			<ul className="flex flex-wrap gap-2 mb-2">
+				{photos.map((photo, index) => (
+					<li key={photo.id}>
+						<PhotoDisplay
+							className={cn(
+								'size-10 rounded-md cursor-pointer border border-transparent',
+								index === activeIndex && 'border-rose-700',
+							)}
+							onClick={() => onPick(index)}
+							photo={photo}
+						/>
+					</li>
+				))}
+			</ul>
+			<PicPicker huntId={hunt.id} />
+		</div>
+	);
+}
+
+interface ActivePhotoProps {
+	hunterId: number;
+	hunters: HunterSchema[];
+	huntId: number;
+	photo: PhotoSchema;
+}
+
+function ActivePhoto({ hunterId, hunters, huntId, photo }: ActivePhotoProps) {
+	const currentHunter = useMemo(
+		() => hunters.find((hunter) => hunter.id === photo.hunterId),
+		[photo.hunterId, hunters],
+	);
+	const isCurrentHunter = currentHunter?.id === hunterId;
+	return (
+		<div className="mb-2 rounded-md overflow-hidden relative">
+			<PhotoDisplay photo={photo} />
+			{!!currentHunter && !isCurrentHunter && (
+				<span className="absolute right-0 bottom-0 p-2 text-white text-sm flex gap-2 items-center bg-black/40 rounded-tl-md">
+					Uploaded by:
+					<Avatar hunter={currentHunter} link />
+				</span>
+			)}
+			{isCurrentHunter && (
+				<DeletePhotoButton huntId={huntId} photoId={photo.id} />
+			)}
+		</div>
+	);
+}
+
+interface DeletePhotoButtonProps {
+	huntId: number;
+	photoId: number;
+}
+function DeletePhotoButton({ huntId, photoId }: DeletePhotoButtonProps) {
+	const queryClient = useQueryClient();
+	const { mutate } = useMutation({
+		mutationFn: () =>
+			fetch(`/api/hunts/${huntId}/photos?photoId=${photoId}`, {
+				method: 'DELETE',
+			}),
+		onSuccess: () => queryClient.invalidateQueries({ queryKey: ['hunts'] }),
+	});
+	return (
+		<Button
+			className="absolute right-2 bottom-2"
+			onClick={() => mutate()}
+			size="icon"
+			variant="destructive"
+		>
+			<Trash />
+		</Button>
+	);
+}
+
+export function PicPicker({ huntId }: Pick<ActivePhotoProps, 'huntId'>) {
 	const queryClient = useQueryClient();
 	const handleCrop = useCallback(
 		async (image: Blob) => {
@@ -30,7 +144,7 @@ export function HuntPics({ huntId }: { huntId: number }) {
 		[huntId, queryClient],
 	);
 	const button = (
-		<Button variant="ghost">
+		<Button className="w-full" variant="secondary">
 			Upload photos
 			<Camera />
 		</Button>
