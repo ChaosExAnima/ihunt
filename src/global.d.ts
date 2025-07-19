abb53a1ebd35c638663fd0ee4d9e61762fafcc5c
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-07-19 22:04:06 +0200

    fix config and make constants constant

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,13 @@
 /// <reference types="vite/client" />
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
+
+interface ImportMetaEnv {
+	readonly VITE_IMAGE_HOST: string;
+}
+
+interface ViteTypeOptions {
+	strictImportMetaEnv: unknown;
+}
