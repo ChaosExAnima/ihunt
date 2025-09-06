a125eb5fabf325bc399349af8996f8fada718846
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-09-06 17:15:32 +0200

    remove welcome component and wire up login

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,7 +1,10 @@
 import { useMutation } from '@tanstack/react-query';
 import { createFileRoute, redirect, useRouter } from '@tanstack/react-router';
+import { SubmitHandler, useForm } from 'react-hook-form';
 
-import Welcome from '@/components/welcome';
+import Header from '@/components/header';
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { trpc } from '@/lib/api';
 
 /* eslint-disable perfectionist/sort-objects */
@@ -32,6 +35,10 @@ export const Route = createFileRoute('/')({
 });
 /* eslint-enable perfectionist/sort-objects */
 
+interface AuthForm {
+	password: string;
+}
+
 function Index() {
 	const router = useRouter();
 	const { isPending, mutate } = useMutation(
@@ -41,5 +48,56 @@ function Index() {
 			},
 		}),
 	);
-	return <Welcome loggingIn={isPending} logInAction={() => mutate()} />;
+	const {
+		formState: { errors },
+		handleSubmit,
+		register,
+	} = useForm<AuthForm>();
+	const onSubmit: SubmitHandler<AuthForm> = (data) => mutate(data);
+	return (
+		<main className="p-4 flex flex-col gap-4 grow">
+			<Header className="text-center">iHunt</Header>
+			<p className="text-xl text-center">
+				Welcome to iHunt Alpha Access!
+			</p>
+			<p className="text-stone-600">Enter your login code below:</p>
+			<form
+				className="grow flex flex-col gap-4"
+				// eslint-disable-next-line @typescript-eslint/no-misused-promises
+				onSubmit={handleSubmit(onSubmit)}
+			>
+				<Input
+					autoFocus
+					{...register('password', { minLength: 3, required: true })}
+					placeholder="Password"
+					type="password"
+				/>
+				{errors.password && (
+					<p className="text-rose-600 text-sm">
+						{errors.password.type === 'minLength' &&
+							'Passcode must be at least 3 characters.'}
+						{errors.password.type === 'required' &&
+							'Passcode is required.'}
+					</p>
+				)}
+				<Button
+					disabled={isPending}
+					size="lg"
+					type="submit"
+					variant="success"
+				>
+					Log In
+				</Button>
+			</form>
+			<p className="text-xs text-muted text-justify">
+				No unauthorized use. If you have not been invited to install
+				this application please immediately remove it from your device.
+				Usage of this application does not constitute an endorsement of
+				its content or purpose by iHunt or its affiliates. Misuse of
+				this application or other violation of our terms of service may
+				result in immediate app deletion, revocation of access, and/or
+				potential legal action at iHunt's sole discretion.
+			</p>
+		</main>
+	);
 }
