a6e647cb4fa894c4d7e2be0a6554817c2f376be3
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-12-22 23:19:00 +0100

    fix deps and eslint

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,15 +1,19 @@
 import js from '@eslint/js';
+import pluginQuery from '@tanstack/eslint-plugin-query';
 import pluginRouter from '@tanstack/eslint-plugin-router';
 import perfectionist from 'eslint-plugin-perfectionist';
 import pluginPrettier from 'eslint-plugin-prettier/recommended';
 import pluginReact from 'eslint-plugin-react';
 import { defineConfig } from 'eslint/config';
+import reactHooks from 'eslint-plugin-react-hooks';
 import globals from 'globals';
 import tseslint from 'typescript-eslint';
 
 export default defineConfig([
 	...pluginRouter.configs['flat/recommended'],
+	pluginQuery.configs['flat/recommended'],
 	perfectionist.configs['recommended-natural'],
+	reactHooks.configs.flat.recommended,
 	{
 		extends: [js.configs.recommended],
 		files: ['**/*.{js,mjs,cjs,ts,mts,cts,jsx,tsx}'],
