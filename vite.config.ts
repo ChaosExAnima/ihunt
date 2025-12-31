02d516f18ce6f37549713234aeb7d5838f60be97
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2025-12-31 15:22:14 +0100

    set up Vite PWA

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite';
 import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
 	plugins: [
@@ -14,6 +15,26 @@ export default defineConfig({
 		}),
 		tailwindcss(),
 		react(),
+		VitePWA({
+			manifest: {
+				description: 'We help you hunt for success!',
+				icons: [
+					{
+						sizes: '192x192',
+						src: 'android-chrome-192x192.png',
+						type: 'image/png',
+					},
+					{
+						sizes: '512x512',
+						src: 'android-chrome-512x512.png',
+						type: 'image/png',
+					},
+				],
+				name: 'iHunt',
+				theme_color: 'oklch(51.4% 0.222 16.935)',
+			},
+			registerType: 'autoUpdate',
+		}),
 	],
 	resolve: {
 		alias: [
