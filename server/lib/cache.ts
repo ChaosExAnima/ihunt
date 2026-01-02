eb41fd225707f9b488038c687c51910514521a34
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-01-02 22:42:31 +0100

    update iterator function to leverage Redis scan

--- server/lib/cache.ts
+++ server/lib/cache.ts
@@ -8,25 +8,52 @@ import { isDev } from '@/lib/utils';
 import { config } from './config';
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any -- Use any as a default.
-let store: KeyvRedis<any> | Map<string, any> = new Map();
+let store: KeyvRedis<any> | null = null;
 if (config.redisUrl) {
-	console.log('Using Redis at', config.redisUrl);
-	store = new KeyvRedis(`redis://${config.redisUrl}`);
+	store = new KeyvRedis(`redis://${config.redisUrl}`, {
+		keyPrefixSeparator: ':',
+	});
 }
+
+const namespace = isDev() ? 'ihunt-dev' : 'ihunt';
 
 export const keyval = new Keyv({
 	deserialize: superjson.parse,
-	namespace: isDev() ? 'ihunt-dev' : 'ihunt',
+	namespace,
 	serialize: superjson.stringify,
-	store,
+	store: store ?? new Map(),
+	useKeyPrefix: false,
 });
 
-export function cacheIterator<TValue>(key: string) {
-	if (!keyval.iterator) {
+export async function* cacheIterator<TValue>(
+	keyPrefix: string,
+): AsyncGenerator<[string, TValue], void> {
+	// KeyV's iterator function, despite the arg, doesn't actually filter by prefix.
+	// See: https://github.com/jaredwray/keyv/issues/1289
+	const iterator = store?.iterator?.bind(store) ?? keyval.iterator;
+	if (!iterator) {
 		throw new Error('Cache backend does not support iterators');
 	}
 
-	return keyval.iterator(key) as AsyncGenerator<[string, TValue], void>;
+	// We need to insert the namespace manually if we're directly using the store.
+	const nsKeyPrefix = store ? `${namespace}:${keyPrefix}` : {};
+	for await (const [key, value] of iterator<TValue>(
+		nsKeyPrefix,
+	) as AsyncGenerator<[string, TValue], void>) {
+		// If we're using the store directly, we need to manually deserialize.
+		if (store) {
+			const data = await keyval.deserializeData<TValue>(value as string);
+			if (data?.value) {
+				// Keys obtained using scan need to be prefixed again.
+				yield [`${keyPrefix}:${key}`, data.value];
+			}
+		}
+
+		// If we're using a map, just check the key is prefixed correctly.
+		if (key.startsWith(keyPrefix)) {
+			yield [key, value];
+		}
+	}
 }
 
 export function getCached<TSchema extends z.ZodType>(
