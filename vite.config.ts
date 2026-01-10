43ddb23cafe8a6f8d694ec98d990cd3391e5ceb9
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-01-10 15:56:59 +0100

    fix manifest images, add ihunt SVG logo

--- vite.config.ts
+++ vite.config.ts
@@ -35,12 +35,12 @@ export default defineConfig({
 				icons: [
 					{
 						sizes: '192x192',
-						src: 'android-chrome-192x192.png',
+						src: '/public/android-chrome-192x192.png',
 						type: 'image/png',
 					},
 					{
 						sizes: '512x512',
-						src: 'android-chrome-512x512.png',
+						src: '/public/android-chrome-512x512.png',
 						type: 'image/png',
 					},
 				],
