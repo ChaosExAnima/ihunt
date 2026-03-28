9132b61bcf5f0983a2ae6548fbe4d22684803376
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-03-28 02:54:31 +0100

    fix bug with input pattern

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -4,7 +4,7 @@ export const Locale = 'de-DE';
 export const Currency = 'EUR';
 
 export const ACCESS_CODE_CHAR_COUNT = 3;
-export const ACCESS_CODE_REGEX = /^[a-z][0-9]{1,2}$/i;
+export const ACCESS_CODE_REGEX = /^[a-zA-Z][0-9]{1,2}$/;
 export const SESSION_COOKIE_NAME = 'ihunt-session';
 
 export const HuntStatus = {
