7c40d6a1c04fdb73c17532524cf0aecb901c8d1e
ChaosExAnima <ChaosExAnima@users.noreply.github.com>
2026-04-04 22:07:07 +0200

    add button to clear caches

--- src/routes/debug.tsx
+++ src/routes/debug.tsx
@@ -1,10 +1,10 @@
 import { useMutation, useQuery } from '@tanstack/react-query';
 import { createFileRoute } from '@tanstack/react-router';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 
+import { ActionButton } from '@/components/action-button';
 import { BackButton } from '@/components/back-button';
 import { Header } from '@/components/header';
-import { Button } from '@/components/ui/button';
 import { apiDebug, trpc } from '@/lib/api';
 
 export const Route = createFileRoute('/debug')({
@@ -15,7 +15,7 @@ const none = <em className="text-muted">None</em>;
 
 function RouteComponent() {
 	const { data } = useQuery(trpc.auth.me.queryOptions());
-	const { lanServer, lanWorking } = useMemo(apiDebug, []);
+	const { lanServer, lanWorking } = apiDebug();
 
 	const { queryClient } = Route.useRouteContext();
 	const { isPending: loggingOut, mutate: logOut } = useMutation(
@@ -29,6 +29,30 @@ function RouteComponent() {
 		logOut();
 	}, [logOut]);
 
+	const [assetCacheSize, setAssetCacheSize] = useState<number>();
+	const [imageCacheSize, setImageCacheSize] = useState<number>();
+	useEffect(() => {
+		void getCachesSizes().then((sizes) => {
+			setAssetCacheSize(sizes['assets']);
+			setImageCacheSize(sizes['images']);
+		});
+	}, []);
+
+	const [cachePending, setCachePending] = useState(false);
+	const handleClearCaches = useCallback(() => {
+		if (cachePending) {
+			return;
+		}
+		setCachePending(true);
+		void clearAllCaches().then(async () => {
+			setCachePending(false);
+
+			const sizes = await getCachesSizes();
+			setAssetCacheSize(sizes['assets']);
+			setImageCacheSize(sizes['images']);
+		});
+	}, [cachePending]);
+
 	return (
 		<main className="flex grow flex-col gap-4 p-4">
 			<Header>Debug information</Header>
@@ -37,17 +61,48 @@ function RouteComponent() {
 				<li>LAN Server: {lanServer ?? none}</li>
 				<li>LAN Active: {lanWorking ? 'Yes' : 'No'}</li>
 				<li>Hunter ID: {data?.hunter.id ?? none}</li>
+				<li>Asset cache size: {assetCacheSize ?? none}</li>
+				<li>Image cache size: {imageCacheSize ?? none}</li>
 			</ul>
-			{data && (
-				<Button
+			{(assetCacheSize || imageCacheSize) && (
+				<ActionButton
 					variant="destructive"
-					disabled={loggingOut}
+					updating={cachePending}
+					onClick={handleClearCaches}
+				>
+					Clear app caches
+				</ActionButton>
+			)}
+			{data && (
+				<ActionButton
+					variant="destructive"
+					updating={loggingOut}
 					onClick={handleLogOut}
 				>
 					Log out
-				</Button>
+				</ActionButton>
 			)}
 			<BackButton />
 		</main>
 	);
+}
+
+async function getCachesSizes() {
+	const list = await caches.keys();
+	const cacheMap: Record<string, number> = {};
+	for (const name of list) {
+		const cache = await caches.open(name);
+		const keys = await cache.keys();
+		cacheMap[name.includes('precache') ? 'assets' : name] = keys.length;
+	}
+	return cacheMap;
+}
+
+async function clearAllCaches() {
+	const list = await caches.keys();
+	for (const name of list) {
+		if (name === 'images' || name.includes('precache')) {
+			await caches.delete(name);
+		}
+	}
 }
